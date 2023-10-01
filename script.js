d5f44db19ddd7333f3c504bc59b27f7f4a4d7dc7
anacarolinabmv <141560459+anacarolinabmv@users.noreply.github.com>
2023-10-01 16:30:24 +0200

    Implement copy password to clipboard

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const sliderEl = document.querySelector('#range');
 const sliderValue = document.querySelector('.length__value');
 
+const btnCopy = document.querySelector('.btn-copy');
+const passwordEl = document.querySelector('.password');
+
 const renderPasswordLength = function (e) {
   const tempSliderValue = e.target.value;
   sliderValue.textContent = tempSliderValue;
@@ -12,6 +15,12 @@ const renderPasswordLength = function (e) {
   sliderEl.style.background = `linear-gradient(to right, #a4ffaf ${progress}%, #18171f ${progress}%)`;
 };
 
+//EVENT LISTENERS
+
 sliderEl.addEventListener('input', (e) => {
   renderPasswordLength(e);
+});
+
+btnCopy.addEventListener('click', function () {
+  navigator.clipboard.writeText(passwordEl.textContent);
 });
