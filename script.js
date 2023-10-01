d4a3cc091bcb1351d81754bdc69f77a4d96c180f
anacarolinabmv <141560459+anacarolinabmv@users.noreply.github.com>
2023-10-01 16:42:56 +0200

    Update copy password function

--- script.js
+++ script.js
@@ -4,7 +4,17 @@ const sliderEl = document.querySelector('#range');
 const sliderValue = document.querySelector('.length__value');
 
 const btnCopy = document.querySelector('.btn-copy');
+const copiedTextEl = document.querySelector('.copied');
 const passwordEl = document.querySelector('.password');
+
+const copyPassword = function () {
+  navigator.clipboard.writeText(passwordEl.textContent);
+  copiedTextEl.classList.add('show');
+
+  setTimeout(() => {
+    copiedTextEl.classList.remove('show');
+  }, 2000);
+};
 
 const renderPasswordLength = function (e) {
   const tempSliderValue = e.target.value;
@@ -21,6 +31,4 @@ sliderEl.addEventListener('input', (e) => {
   renderPasswordLength(e);
 });
 
-btnCopy.addEventListener('click', function () {
-  navigator.clipboard.writeText(passwordEl.textContent);
-});
+btnCopy.addEventListener('click', copyPassword);
